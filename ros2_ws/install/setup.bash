57668284355730c424a76aa853f349cd5ce48bc2
Peter Chang <tsyuen@gmail.com>
2026-01-06 13:41:25 +0800

    change setup into two parts

--- ros2_ws/install/setup.bash
+++ ros2_ws/install/setup.bash
@@ -1,5 +1,12 @@
 # test overlay bash command installation
 
-
-echo -e "\e[34mRuntime is here!\e[0m"
 echo -e "\e[31mNo special overlay sourced!!!\e[0m"
+
+# ros domain id assignment
+if [ -z "ROS_DOMAIN_ID" ]; then
+  export ROS_DOMAIN_ID=10
+  echo -e "\e[34mRuntime is here!\e[0m"
+fi
+
+echo -e "\e[32mROS DOMAIN ID is:\e[0m"
+echo $ROS_DOMAIN_ID
--- ros2_ws/setup.sh
+++ ros2_ws/setup.sh
@@ -7,16 +7,12 @@ WS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Source ROS 2 base
 source /opt/ros/jazzy/setup.bash
 
+# Environment check
+~/bin/check_dev_env.sh
+
 # Optional: source workspace overlay (future-proof)
 if [ -f "${WS_DIR}/install/setup.bash" ]; then
   source "${WS_DIR}/install/setup.bash"
 fi
 
-# Environment check
-~/bin/check_dev_env.sh
-
-# ros domain id assignment
-if [ -z "ROS_DOMAIN_ID" ]; then
-  export ROS_DOMAIN_ID=10
-fi
 
