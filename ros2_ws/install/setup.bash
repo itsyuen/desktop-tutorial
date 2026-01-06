ff4b65b59e199de3ebb52cdf2565f988b2426ad8
itsyuen <tsyuen@gmail.com>
2026-01-06 21:46:43 +0800

    new path check

--- ros2_ws/install/setup.bash
+++ ros2_ws/install/setup.bash
@@ -1,14 +1,14 @@
 # test overlay bash command installation
 
-echo -e "\e[31mNo special overlay sourced!!!\e[0m"
+echo -e "\e[31mCurrent ROS2 runtime setup!!!\e[0m"
+echo -e "\e[34mOverlay runtime is here!\e[0m"
 
 # ros domain id assignment
 if [ -z "$ROS_DOMAIN_ID" ]; then
   export ROS_DOMAIN_ID=10
-  echo -e "\e[34mRuntime is here!\e[0m"
+  echo -e "\e[32mROS DOMAIN ID is:\e[0m$ROS_DOMAIN_ID"
 fi
 
-echo -e "\e[32mROS DOMAIN ID is:\e[0m$ROS_DOMAIN_ID"
-
-export PATH="~/GitHub/desktop-tutorial/:$PATH"
+export PATH="~/GitHub/desktop-tutorial/ros2_ws:$PATH"
+echo "The following paths are shortcut:"
 echo $PATH
