8a36d385db24a2035b3dafed9b244c7b1ea0606d
Peter from Dell <tsyuen@gmail.com>
2026-01-26 13:57:41 +0800

    make changes and practice cpp

---
+++ cpp/scope_basics/test.cpp
@@ -0,0 +1,19 @@
+// my restart cpp program
+
+#include <iostream>
+using namespace std;
+
+namespace demo {
+    int add(int a, int b){
+        return a +  b;
+    }
+}
+
+int main ()
+{
+    cout << "Hello world!! again";
+
+    std::cout << demo::add(2, 3) << std::endl;
+
+    return 0;
+}
