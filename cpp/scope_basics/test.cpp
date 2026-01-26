a2ae1c3f1a52d6e763bdec8d2483194a554ac574
Peter I-Tsyuen Chang <tsyuen@gmail.com>
2026-01-26 23:03:43 +0800

    update instructor inline

--- cpp/scope_basics/test.cpp
+++ cpp/scope_basics/test.cpp
@@ -9,18 +9,30 @@ namespace demo {
     // use std::library of array kind to make sure
     // use pointers to practice using a full object to compare apples and oranges.
 
-    int add(int a, int b){
-        return a +  b;
+    class Fruit {
+        public:
+        // constructor
+            Fruit(double w, std::string c) : weight(w), color(c) {}
+        // member functions
+            double getWeight(){ return weight;}
+            std::string getColor(){ return color;}
+
+            void setWeight(double a){ weight = a; }
+            void setColor(std::string c){ color = c; }
+        private:
+            double weight;
+            std::string color;
     }
 
-
-}
+    void Fruit::setWeight(double a){
+        weight = a;
+    }
 
 int main ()
 {
-    cout << "Hello world!! again" << endl;
+    cout << "Fruit calss test!!" << endl;
 
-    std::cout << demo::add(2, 3) << std::endl;
+    //std::cout << demo::add(2, 3) << std::endl;
 
     return 0;
 }
